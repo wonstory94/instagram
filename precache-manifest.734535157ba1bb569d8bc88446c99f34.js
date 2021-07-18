fc3c983892c139b9122d4aab446e8c1ad4ba2ebd
wonstory94 <37845783+wonstory94@users.noreply.github.com>
2021-07-18 18:46:50 +0900

    vuestagram

---
+++ precache-manifest.734535157ba1bb569d8bc88446c99f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdcb3f5254e05157fc3",
+    "url": "/instagram/css/app.3e2407c4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/instagram/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "95a803e1d64667dcad9431909b124aea",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "7fdcb3f5254e05157fc3",
+    "url": "/instagram/js/app.466d322e.js"
+  },
+  {
+    "revision": "05bdbbab5976b32890c8",
+    "url": "/instagram/js/chunk-vendors.89c11e4b.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/instagram/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/instagram/robots.txt"
+  }
+]);
